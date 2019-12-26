71a1c498948860f7940518b38e0fef12e32cb845
Dushyant Juneja <juneja.dushyant@gmail.com>
2019-12-26 18:04:22 -0500

    Update webpack configurations to include better configurations

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,38 +1,56 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
     stats: {
 	logging: 'verbose',
 	colors: true,
     },
+    entry: './src/client/index.js',
+    output: {
+	libraryTarget: 'var',
+	library: 'Client'
+    },
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+	rules: [
+	    {
+		// Run all custom JS in project through
+		// babel. Otherwise webpack will move advanced ES
+		// syntaxes to browser distribution and will create
+		// issues there.
+		test: /\.js$/,
+		exclude: /node_modules/,
+		loader: "babel-loader"
+	    },
+	    {
+		enforce: 'pre', // This comes before babel-loader
+		test: /.js$/,
+		exclude: /node_modules/,
+		loader: 'eslint-loader',
+		options: {}
+	    },
+	    {
+		test: /\.scss$/,
+		use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+	    }
+	]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
-    ]
-}
+	new HtmlWebPackPlugin({
+	    template: './src/client/views/index.html',
+	    filename: './index.html'
+	}),
+	new CleanWebpackPlugin({
+	    dry: true,
+	    verbose: true,
+	    cleanStaleWebpackAssets: true,
+	    protectWebpackAssets: false
+	}),
+	new BundleAnalyzerPlugin(),
+    ],
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,52 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// Service workers added in prod to allow offline access.
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
     mode: 'production',
+    entry: './src/client/index.js',
+    optimization: {
+	minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin()]
+    },
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+	rules: [
+	    {
+		// Run all custom JS in project through
+		// babel. Otherwise webpack will move advanced ES
+		// syntaxes to browser distribution and will create
+		// issues there.
+		test: /\.js$/,
+		exclude: /node_modules/,
+		loader: "babel-loader"
+	    },
+	    {
+		enforce: 'pre', // This comes before babel-loader
+		test: /.js$/,
+		exclude: /node_modules/,
+		loader: 'eslint-loader',
+		options: {}
+	    },
+	    {
+		// We use the loader supplied by plugin instead of
+		// style-loader here. This enables putting the styles
+		// as a stylesheet in the HTML, as opposed to styles
+		// done via JS in case of style-loader.
+		test: /\.scss$/,
+		use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+	    }
+	]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
+	new HtmlWebPackPlugin({
+	    template: './src/client/views/index.html',
+	    filename: './index.html'
+	}),
+	new MiniCssExtractPlugin({filename: '[name].css'}),
+	new WorkboxPlugin.GenerateSW(),
     ]
 }
